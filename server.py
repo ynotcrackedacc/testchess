9a49ce47a68e207bcc29b6be5c9db976bc391fa5
ynotcrackedacc <ynotcrackedacc@gmail.com>
2025-03-04 23:44:44 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
+from fastapi.staticfiles import StaticFiles
+import PyChessBot
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='static', html=True), name='static')
+
+
+@app.get('/getMove', response_class=PlainTextResponse)
+async def getMove(fen: str):
+    bot = PyChessBot.ChessBot()
+    bot.setFen(fen)
+    return bot.findMove(3)
+
+uvicorn.run(app, host='0.0.0.0', port=8000)
