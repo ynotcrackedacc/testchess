69260825c9d026406069a260a9ca9aa7e0e4a10d
ynotcrackedacc <ynotcrackedacc@gmail.com>
2025-03-05 01:35:38 -0500

    Update server.py

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ import importlib.util
 import sys
 
 # Specify the path to the .so file (extension module)
-so_file_path = "PyChessBot.cpython-312-x86_64-linux-gnu.so"
+so_file_path = "PyChessBot.cpython-310-x86_64-linux-gnu.so"
 module_name = "PyChessBot"
 
 # Load the module
