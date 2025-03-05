4ba408c6b1bcd795629c6042361004d0a6795441
ynotcrackedacc <ynotcrackedacc@gmail.com>
2025-03-05 01:00:10 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,14 +2,22 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 from fastapi.staticfiles import StaticFiles
-import os
+
+import importlib.util
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-print(os.getcwd())
-print(os.listdir())
+# Specify the path to the .so file (extension module)
+so_file_path = "PyChessBot.cp310-win_amd64.pyd"
+module_name = "PyChessBot"
 
-import PyChessBot
+# Load the module
+spec = importlib.util.spec_from_file_location(module_name, so_file_path)
+PyChessBot = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = module
+spec.loader.exec_module(module)
+
+print(PyChessBot)
+
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static', html=True), name='static')
